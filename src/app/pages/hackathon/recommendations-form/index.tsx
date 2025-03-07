1c5783d72c0770f1a977ae8086dbee49d353eea3
Amiola Demilade <amiolademilade@gmail.com>
2025-03-07 09:38:18 +0100

    Layout structure

---
+++ src/app/pages/hackathon/recommendations-form/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RecommendationForm from '@/app/views/hackathon/recommendation-form'
+
+function Hackathon() {
+  return (
+    <div>
+        <RecommendationForm />
+    </div>
+  )
+}
+
+export default Hackathon;
---
+++ src/app/views/hackathon/recommendation-form/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RecommendationForm() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default RecommendationForm
