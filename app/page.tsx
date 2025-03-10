6f0af19869bbe5055205816d6a8dada076fb4c13
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-03-10 16:09:26 +0100

    feat: integrate next-auth for session management in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 import LoginForm from "./_components/LoginForm";
 import Button from "./_components/ui/Button";
 import { X } from "lucide-react";
+import { getServerSession } from "next-auth";
+import { options } from "./api/auth/[...nextauth]/options";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(options);
+
   return (
     <div className="max-w-full">
       <button className="absolute top-0 right-0 h-8 w-8">
@@ -16,6 +20,8 @@ export default function Home() {
         <h1 className="text-2xl font-bold text-gray-900">Welcome Back</h1>
         <p className="text-secondary-50 text-sm">Sign in to your account</p>
         <LoginForm />
+
+        {session ? "hey" : "hi"}
       </div>
     </div>
   );
