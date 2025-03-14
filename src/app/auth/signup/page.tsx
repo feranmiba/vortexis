bcef5c13c8bc070e132b78dfb41b7676bdf0cd3a
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-03-14 11:06:49 +0100

    feat: wrap close button in Link component for improved navigation

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,12 +1,15 @@
 import LoginForm from "@/components/ui/AuthForm";
 import { X } from "lucide-react";
+import Link from "next/link";
 
 export default async function Home() {
   return (
     <div className="sm:max-w-full md:max-w-full">
-      <button className="absolute top-0 right-0 h-8 w-8">
+<Link href="/">
+      <button className="absolute cursor-pointer top-0 right-0 h-8 w-8">
         <X className="h-5 w-5 text-gray-500" />
       </button>
+</Link>
 
       <div className="mt-0 px-8 md:mt-8 md:px-12">
         <h1>Get Started Now</h1>
