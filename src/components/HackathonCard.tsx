0e65490c37f4a39197ded17791ebfad170c9f776
Amiola Demilade <amiolademilade@gmail.com>
2025-03-19 18:41:06 +0100

    icons

--- src/components/HackathonCard.tsx
+++ src/components/HackathonCard.tsx
@@ -1,6 +1,7 @@
 import { StaticImageData } from "next/image";
 import Image from "next/image";
-import { types } from "node:util";
+// import Location from "@/public/assets/icon/mdi_location.svg"
+// import { types } from "node:util";
 import React from "react";
 
 interface CardData {
@@ -37,15 +38,15 @@ const HackathonCard: React.FC<CardData> = ({
   const hoursLeft = Math.max(0, Math.floor((hackathonTime - now) / (1000 * 60 * 60)));
 
   return (
-    <div className="flex  p-5 border rounded-lg shadow-md">
-      <div className="flex gap-4 border-r-2 px-3">
+    <div className="flex gap-10 mt-10  border rounded-lg shadow-md">
+      <div className="flex gap-4 border-r-2 px-3 w-3/5 justify-between py-10">
       <Image src={image} alt={name} width={300} height={200} className="rounded-lg" />
-      <div>
-       <h2 className="text-xl font-semibold">{name}</h2>
-       <p className="font-bold text-red-500 border rounded-full flex justify-center items-center px-3 py-4">Time Left: {hoursLeft} hours</p>
+      <div className="space-y-4">
+       <h2 className="text-xl font-semibold text-[#282828]">{name}</h2>
+       <p className=" text-[#1E1E1E] border rounded-full flex justify-center items-center px-2 py-2">Time Left: {hoursLeft} hours</p>
        <p>{location}</p>
-       <p>{participants} participants</p>
-       <p>Prize: ${prize}</p>
+       <p> <span className="text-[#282828] font-bold"> {participants} </span> participants</p>
+       <p>Prize: <span className="text-[#282828] font-bold">${prize}</span></p>
 
 
        </div>
@@ -53,11 +54,11 @@ const HackathonCard: React.FC<CardData> = ({
 
       </div>
 
-      <div>
+      <div className="space-y-4 mt-10 py-10">
+      <p className=" text-[#1E1E1E] border rounded-full flex justify-center items-center px-2 py-2">Organization: {organization}</p>
       <p>Date: {date}</p>
       <p>Mode: {mode}</p>
       <p>Start Time: {startTime}</p>
-      <p>Organization: {organization}</p>
       <p>Entry: {entry}</p>
       <p>Type: {type}</p>
       </div>
