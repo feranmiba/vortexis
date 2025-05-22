3267903b8e7d312edc85beeccdf00e8e1fd02eb9
Oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-05-22 17:08:35 +0100

    feat: add Deliverables component and integrate into Submission Review page

--- src/app/judges/submission-review/[id]/page.tsx
+++ src/app/judges/submission-review/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Deliverables from "@/components/judgeReview/deliverables";
 import Vote from "@/components/judgeReview/vote";
 import { useState } from "react";
 
@@ -39,26 +40,29 @@ export default function SubmissionReviewPage({
         <p>Reviewing submissions</p>
       </div>
 
-      <div className="bg-[#FFFFFF] my-5 p-3 shadow-md rounded-md border border-[#E4E4E4]">
-        <div className="flex mb-6 mt-2 cursor-pointer gap-4 ">
-          {tabs.map((tab, i) => {
-            return (
-              <div key={i} onClick={() => setActiveTab(tab.tab_no)}>
-                <p
-                  className={`text-center px-7 py-2 ${
-                    activeTab === i + 1
-                      ? "bg-[#605DEC] text-white"
-                      : "bg-[#F4F3FE] text-[#C5C0DB]"
-                  } transition-all duration-300 rounded-md `}
-                >
-                  {tab.name}
-                </p>
-              </div>
-            );
-          })}
-        </div>
+      <div className="bg-[#FFFFFF] my-5  shadow-md rounded-md border border-[#E4E4E4]">
+        <div className="md:w-[55%] px-3 py-8">
+          <div className="flex mb-6 -mt-1.5 cursor-pointer gap-4 ">
+            {tabs.map((tab, i) => {
+              return (
+                <div key={i} onClick={() => setActiveTab(tab.tab_no)}>
+                  <p
+                    className={`text-center px-7 py-2 ${
+                      activeTab === i + 1
+                        ? "bg-[#605DEC] text-white"
+                        : "bg-[#F4F3FE] text-[#C5C0DB]"
+                    } transition-all duration-300 rounded-md `}
+                  >
+                    {tab.name}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
 
-        {activeTab === 1 && <Vote />}
+          {activeTab === 1 && <Vote />}
+          {activeTab === 2 && <Deliverables />}
+        </div>
       </div>
     </div>
   );
